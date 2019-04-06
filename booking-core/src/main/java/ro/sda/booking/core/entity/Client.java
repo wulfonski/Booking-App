a576ef88d7bbe9ba23ddd5c6ca3e256f0cae1cc4
wulfoski <Iosif*farcas88>
2019-04-06 13:06:57 +0300

    created the entity for Client, Host and Property

---
+++ booking-core/src/main/java/ro/sda/booking/core/entity/Client.java
@@ -0,0 +1,72 @@
+package ro.sda.booking.core.entity;
+
+import ro.sda.booking.core.base.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "clients", schema = "booking")
+public class Client extends BaseEntity {
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "contact_no", length = 50)
+    private String contact;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ro.sda.booking.core.entity.Client)) return false;
+        ro.sda.booking.core.entity.Client client = (ro.sda.booking.core.entity.Client) o;
+        return super.getId() == (client.getId()) &&
+                getName().equals(client.getName()) &&
+                getEmail().equals(client.getEmail()) &&
+                getContact().equals(client.getContact());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getEmail(), getContact(), getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + getId() +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
+}
--- booking-core/src/main/java/ro/sda/booking/core/entity/Host.java
+++ booking-core/src/main/java/ro/sda/booking/core/entity/Host.java
@@ -2,10 +2,58 @@ package ro.sda.booking.core.entity;
 
 import ro.sda.booking.core.base.BaseEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "hosts", schema = "booking")
 public class Host extends BaseEntity {
+
+    @Column(name = "hosts", length = 50)
+    private String name;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Host)) return false;
+        Host host = (Host) o;
+        return super.getId() == (host.getId()) &&
+                Objects.equals(getName(), host.getName()) &&
+                Objects.equals(getEmail(), host.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getEmail(), getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Host{" +
+                "id=" + getId() +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
